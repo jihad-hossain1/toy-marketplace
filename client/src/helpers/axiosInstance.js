6fadd1f6537f1772d8d4002dd502d36922c9c5bc
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-08 19:35:58 +0600

    deploy are not work -fix-this-errror-

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-// const BASE_URL = "https://toyproductsserver.vercel.app/api";
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://toyproductsserver.vercel.app/api";
+// const BASE_URL = "http://localhost:5000/api";
 
 const axiosInstance = axios.create();
 
