e7cc01d892bbd785972a984644063c63f817e5e9
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-09 15:45:41 +0600

    fix category

--- server/models/category.js
+++ server/models/category.js
@@ -5,6 +5,10 @@ const categorySchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
 });
 
 const Category = mongoose.model("Category", categorySchema);
