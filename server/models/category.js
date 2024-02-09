734e6bb1a45e9ac1f2ce92d8343ad0e13d581f66
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-09 15:41:27 +0600

    some issue in product category model

--- server/models/category.js
+++ server/models/category.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
-const categorySchema = mongoose.Schema({
+const categorySchema = new mongoose.Schema({
   category: {
     type: String,
     required: true,
   },
 });
 
-const Category =
-  mongoose.models.Category || mongoose.model("Category", categorySchema);
+const Category = mongoose.model("Category", categorySchema);
 
 module.exports = Category;
